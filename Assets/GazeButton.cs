e160c43a7f48048218148675d9d4b260d3de823c
Kanika <Kanika.modi@gmail.com>
2017-08-28 23:29:13 +0530

    Revert bck to previous one

---
+++ Assets/GazeButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GazeButton : MonoBehaviour {
+    public Image Imagebutton;
+	// Use this for initialization
+    public void greencolor()
+    {
+        Imagebutton.color = Color.green;
+    }
+
+    public void redcolor()
+    {
+        Imagebutton.color = Color.red;
+    }
+}
